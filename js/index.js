b7649b5ff916c5320c346ac2a67491dc7dc6c7a7
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-09-01 21:29:05 -0300

    Added input to set line width

--- js/index.js
+++ js/index.js
@@ -2,19 +2,35 @@ document.addEventListener("DOMContentLoaded", ()=> {
   const canvas = document.getElementById("drawing");
   const sideMenu = document.getElementById("side-menu");
   const drawingColor = document.getElementById("drawing-color");
+  const txtLineWidth = document.getElementById("txtLineWidth");
+  let lineWidth = 5;
+
+  txtLineWidth.value = lineWidth;
 
   const ctx = canvas.getContext("2d");
 
-  let coord = {x:0,y:0};
+  let coord = {x:0, y:0};
   let color = "#000";
 
   sideMenu.addEventListener("change", event => {
     color = event.target.value;
   });
 
+  txtLineWidth.addEventListener("change", event => {
+    const newLineWidth = Number.parseInt(event.target.value);   
+    
+    if(newLineWidth > 200){
+      event.preventDefault();
+      txtLineWidth.value = 200;
+      return;
+    }
+    
+    lineWidth = event.target.value;
+  })
+
   const draw = event => {
     ctx.beginPath();
-    ctx.lineWidth = 5;
+    ctx.lineWidth = lineWidth;
     ctx.lineCap = 'round';
     ctx.strokeStyle = color;
     ctx.moveTo(coord.x, coord.y);
